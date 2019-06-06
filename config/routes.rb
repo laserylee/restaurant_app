5799e4c9bd37925411c964a37d457285d5ca1178
laserylee <laserylee2013@gmail.com>
2019-06-06 20:59:46 +0800

    Added routes for StaticPages, generated devise view templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
-
-  get 'static_pages/help'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/adminpanel'
+  get '/about'      , to:'static_pages#home'
+  get '/help'       , to:'static_pages#help'
+  get '/contact'    , to:'static_pages#contact'
+  get '/adminpanel' , to:'static_pages#adminpanel'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
