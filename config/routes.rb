abd3bb53f6ae3485fd0a1851e3244d5911b1736b
laserylee <laserylee2013@gmail.com>
2019-06-06 22:27:12 +0800

    Generated devise controller for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   root 'static_pages#home'
 
   get '/about'      , to:'static_pages#home'
