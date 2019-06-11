de6ad6d7e5e31cdf780bf178d5db649162ab50aa
laserylee <laserylee2013@gmail.com>
2019-06-11 21:32:08 +0800

    Added model Order

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/about'      , to:'static_pages#home'
+  get '/about'      , to:'static_pages#about'
   get '/help'       , to:'static_pages#help'
   get '/contact'    , to:'static_pages#contact'
   get '/adminpanel' , to:'static_pages#adminpanel'
