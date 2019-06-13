e5cbe522c57ef4fca75acaf6b085d964c6747c8e
laserylee <laserylee2013@gmail.com>
2019-06-13 18:00:05 +0800

    Save pickup time and set order status

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,17 @@ class OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
+    y = params[:order]["pickup_time(1i)"]
+    mo = params[:order]["pickup_time(2i)"]
+    d = params[:order]["pickup_time(3i)"]
+    h = params[:order]["pickup_time(4i)"]
+    mi = params[:order]["pickup_time(5i)"]
+    @pickup_time = Time.new(y,mo,d,h,mi)
+    if @pickup_time > (Time.now + 1.hours)
+      @order.pickup_time = @pickup_time
+      @order.order_status_id = 2
+      @order.save!
+    end 
     byebug
   end
 
